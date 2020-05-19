03f7989460322cb0154a8e151ca7f6e0d62f1f4d
Tolutronics <toluxy100@gmail.com>
2020-05-19 14:50:24 -0700

    new myschool

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './child.jpg'
+import * as signalR from '@aspnet/signalr';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="cover">
+      
+      <div className="image">
+     <div className="overlay">
+     <div className="top">
+       <ul className="social">
+         <li>1</li>
+         <li>2</li>
+         <li>3</li>
+         <li>4</li>
+       </ul>
+       <div className="spacer"></div>
+       <ul className="social">
+         <li>+2347031234126</li>
+         <li>toluxy100@gmail.com</li>
+       </ul>
+     </div>
+       <div className="container">
+         
+     <div className="header">
+       <div className="logoa">
+         <div className="logob"></div>
+       </div>
+       <h2 >MYSCHOOL</h2>
+      <div className="spacer"></div>
+        <ul className="menu"> 
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>EVENT</li>
+          <li>ADMISSIONS</li>
+          <li>CLASS</li>
+          <li>ALUMNI</li>
+          <li>STAFF</li>
+          <li>CONTACT</li>
+        </ul>
+
+      </div>
+      <div className="quote">
+<h1>WE ENSURE BETTER EDUCATION FOR A BETTER WORLD</h1>
+<p>In the history of modern astronomy, there is probably 
+  no one greater leap forward than the building and launch 
+  of the space telescope known as the Hubble.</p>
+  <button>GET STARTED</button>
+
+      </div>
+      </div>
+      {/* <input
+        type="text"
+        value={'tolu'}
+        
+      />
+
+      <button>Send</button> */}
+     </div>
+     
+      </div>
+
+        
     </div>
   );
 }
